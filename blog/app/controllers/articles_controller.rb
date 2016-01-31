8c278848b024f4e4f1b127a946efb26d075b7c7e
abdullahvelioglu <abdullahvelioglu.295086@hotmail.com>
2016-01-31 17:04:15 +0200

    blog edit update link

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,15 +1,44 @@
 class ArticlesController < ApplicationController
   def new
+    @article=Article.new
   end
 
   def index
+    @articles = Article.all
   end
 
   def create
-    @article = Article.new(params[:article])
+    @article = Article.new(article_params)
     @article.save
-    redirect_to @article
+    if @article.save
+  redirect_to @article
+else
+  render 'new'
+end
   end
 
+def show
+   @article = Article.find(params[:id])
+end
 
+def edit
+  @article = Article.find(params[:id])
+end
+
+def update
+  @article = Article.find(params[:id])
+
+  if @article.update(article_params)
+    redirect_to @article
+  else
+    render 'edit'
+  end
+end
+
+
+
+private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
 end
